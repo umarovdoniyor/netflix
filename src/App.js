4792b5821bcb36275f2db48701de10a10b0a3215
Doniyor Umarov <doniyorumarov@Doniyors-MacBook-Pro.local>
2021-09-16 17:41:39 +0900

    Basic routing implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import { JumbotronContainer } from "./containers/jumbotron";
+import * as ROUTES from "./constants/routes";
+
+import { Home, Signin, Signup, Browse } from "./pages";
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <Signin />
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
