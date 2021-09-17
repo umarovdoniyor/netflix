45a938b77a7bb083a11fd0ea7a7469d4dba5b4b4
Doniyor Umarov <doniyorumarov@Doniyors-MacBook-Pro.local>
2021-09-18 00:32:56 +0900

    Routing re-implemented. Protected Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 import * as ROUTES from "./constants/routes";
 
 import { Home, Signin, Signup, Browse } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 export default function App() {
+  const user = null;
   return (
     <Router>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <Signin />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+        >
+          <Signin />
+        </IsUserRedirect>
+
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+        >
+          <Signup />
+        </IsUserRedirect>
+
+        {/* <Route exact path={ROUTES.SIGN_UP}>
         <Signup />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
+      </Route> */}
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+        >
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
